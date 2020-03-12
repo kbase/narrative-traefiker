034a2a9a444190bab40b988c812ba7e34cfa19fd
Steve Chan <sychan168@gmail.com>
2020-03-12 11:58:37 -0700

    Move scheduler initialization to proper location

--- app.py
+++ app.py
@@ -106,6 +106,10 @@ def setup_app(app):
         logger.critical("Failed validation of docker or rancher configuration")
         raise(ex)
     logger.info({'message': "container management mode set to: {}".format(cfg['mode'])})
+    logger.info({"message": "Starting scheduler", "reaper_timeout_sec": cfg['reaper_timeout_secs'],
+                    "reaper_sleep_secs": cfg['reaper_sleep_secs']})
+    scheduler.start()
+    scheduler.add_job(reaper, 'interval', seconds=cfg['reaper_sleep_secs'], id='reaper')
 
 
 def reload_msg(narrative, wait=0):
@@ -250,10 +254,6 @@ setup_app(app)
 if __name__ == '__main__':
 
     if cfg['mode'] is not None:
-        logger.info({"message": "Starting scheduler", "reaper_timeout_sec": cfg['reaper_timeout_secs'],
-                     "reaper_sleep_secs": cfg['reaper_sleep_secs']})
-        scheduler.start()
-        scheduler.add_job(reaper, 'interval', seconds=cfg['reaper_sleep_secs'], id='reaper')
         app.run()
     else:
         logger.critical({"message": "No container management configuration. Please set docker_url or rancher_* environment variable appropriately"})
