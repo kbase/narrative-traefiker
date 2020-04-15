2f60e47c74a3cad449e28e68f1d94757e4541cd1
Steve Chan <sychan168@gmail.com>
2020-04-15 12:00:59 -0700

    Don't need microsecond resolution on timestamp, get rid of commented out line

--- CustomJsonFormatter.py
+++ CustomJsonFormatter.py
@@ -7,7 +7,7 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
         super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
         if not log_record.get('timestamp'):
             # this doesn't use record.created, so it is slightly off
-            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            now = datetime.utcnow().isoformat(timespec='milliseconds')+"Z"
             log_record['timestamp'] = now
         if log_record.get('level'):
             log_record['level'] = log_record['level'].upper()
