dd4196935500b0d090756135cda2f3fa4f7422a1
Steve Chan <sychan168@gmail.com>
2020-04-23 11:20:15 -0700

    Added dash between account and hash

--- app.py
+++ app.py
@@ -303,8 +303,8 @@ def clean_userid( userid: str) -> str:
     hash = hashlib.sha1(userid.encode()).hexdigest()
     hash = hash[:6]
     cleaned = re.sub('[\._-]+', '-', userid)
-    max_len = 63 - len(cfg['container_name']) - len(hash)
-    cleaned = cleaned[:max_len]+hash
+    max_len = 62 - len(cfg['container_name']) - len(hash)
+    cleaned = "{}-{}".format(cleaned[:max_len], hash)
     return(cleaned)
 
 
