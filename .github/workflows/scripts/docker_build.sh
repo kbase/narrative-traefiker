f09124c107602f50848b3940921eea36779713e7
JS Fillman <jsfillman@lbl.gov>
2020-08-25 18:34:10 -0700

    Unlikely first test of local -> GH Actions build

--- .github/workflows/scripts/docker_build.sh
+++ .github/workflows/scripts/docker_build.sh
@@ -1,18 +1,14 @@
 #! /usr/bin/env bash
 
-export BRANCH=$(git symbolic-ref --short HEAD)
 export DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-export IMAGE_NAME=narrative-traefiker
-export COMMIT=$(git rev-parse --short HEAD)
-export PR=9001
+export COMMIT=$(echo $SHA | cut -c -7)
 
-docker login -u jsfillman -p $TOKEN docker.pkg.github.com
+docker login -u $DOCKER_ACTOR -p $DOCKER_TOKEN docker.pkg.github.com
 docker build --build-arg BUILD_DATE=$DATE \
              --build-arg COMMIT=$COMMIT \
-             --build-arg BRANCH=$BRANCH \
+             --build-arg BRANCH=$GITHUB_HEAD_REF \
              --build-arg PULL_REQUEST=$PR \
              --label us.kbase.vcs-pull-req=$PR \
-             -t $IMAGE_NAME"-test" .
-docker tag $IMAGE_NAME"-test" docker.pkg.github.com/jsfillman/$IMAGE_NAME/$IMAGE_NAME"-test"
-docker push docker.pkg.github.com/jsfillman/$IMAGE_NAME/$IMAGE_NAME"-test"
+             -t docker.pkg.github.com/$MY_APP"-test" .
+docker push docker.pkg.github.com/$MY_APP"-test"
