db3ba81a5c54d1e3d7273cd4bb4410f906970e33
JS Fillman <jsfillman@lbl.gov>
2020-08-25 18:49:04 -0700

    Moving all var declarations to Actions workflow, for lulz

--- .github/workflows/scripts/docker_build.sh
+++ .github/workflows/scripts/docker_build.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-export DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-export COMMIT=$(echo $SHA | cut -c -7)
+# export DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+# export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+# export COMMIT=$(echo $SHA | cut -c -7)
 
 docker login -u $DOCKER_ACTOR -p $DOCKER_TOKEN docker.pkg.github.com
 docker build --build-arg BUILD_DATE=$DATE \
