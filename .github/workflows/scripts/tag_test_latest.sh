40c228f255b37387177a267cc46cd72edcbda572
Jason S Fillman <6155956+jsfillman@users.noreply.github.com>
2020-08-26 19:17:59 -0700

    Syntax fix

--- .github/workflows/scripts/tag_test_latest.sh
+++ .github/workflows/scripts/tag_test_latest.sh
@@ -6,6 +6,6 @@ export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export COMMIT=$(echo "$SHA" | cut -c -7)
 
 docker login -u "$DOCKER_ACTOR" -p "$DOCKER_TOKEN" docker.pkg.github.com
-docker pull docker.pkg.github.com/"$MY_APP":"pr-""$PR" .
+docker pull docker.pkg.github.com/"$MY_APP":"pr-""$PR"
 docker tag docker.pkg.github.com/"$MY_APP":"pr-""$PR" docker.pkg.github.com/"$MY_APP":"latest"
 docker push docker.pkg.github.com/"$MY_APP":"latest"
