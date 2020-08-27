4bc9ee986505c2e5dbe56b9ff68c84161c40f1eb
Jason S Fillman <6155956+jsfillman@users.noreply.github.com>
2020-08-27 13:13:05 -0700

    Update tag_prod_latest.sh

--- .github/workflows/scripts/tag_prod_latest.sh
+++ .github/workflows/scripts/tag_prod_latest.sh
@@ -6,6 +6,6 @@ export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export COMMIT=$(echo "$SHA" | cut -c -7)
 
 docker login -u "$DOCKER_ACTOR" -p "$DOCKER_TOKEN" docker.pkg.github.com
-docker pull docker.pkg.github.com/"$MY_APP":"-rc"
-docker tag docker.pkg.github.com/"$MY_APP":"-rc""$PR" docker.pkg.github.com/"$MY_APP":"latest"
+docker pull docker.pkg.github.com/"$MY_APP":"rc"
+docker tag docker.pkg.github.com/"$MY_APP":"rc" docker.pkg.github.com/"$MY_APP":"latest"
 docker push docker.pkg.github.com/"$MY_APP":"latest"
