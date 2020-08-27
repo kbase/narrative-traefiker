790737b1d5362c5e4d78836c2dbe694dba89d00e
Jason S Fillman <6155956+jsfillman@users.noreply.github.com>
2020-08-27 13:00:05 -0700

    Update build_prodrc_pr.sh

--- .github/workflows/scripts/build_prodrc_pr.sh
+++ .github/workflows/scripts/build_prodrc_pr.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-export MY_APP=$(echo "${GITHUB_REPOSITORY}"/$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}' | sed -e "s/:refs//")"-test")
+export MY_APP=$(echo "${GITHUB_REPOSITORY}"/$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}' | sed -e "s/:refs//"))
 export DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export COMMIT=$(echo "$SHA" | cut -c -7)
@@ -11,5 +11,5 @@ docker build --build-arg BUILD_DATE="$DATE" \
              --build-arg BRANCH="$GITHUB_HEAD_REF" \
              --build-arg PULL_REQUEST="$PR" \
              --label us.kbase.vcs-pull-req="$PR" \
-             -t docker.pkg.github.com/"$MY_APP":"pr-""$PR" .
-docker push docker.pkg.github.com/"$MY_APP":"pr-""$PR"
+             -t docker.pkg.github.com/"$MY_APP":"-rc" .
+docker push docker.pkg.github.com/"$MY_APP":"-rc"
